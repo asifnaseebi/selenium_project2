4e84826e3b6b3b7bc15daf41e45a3bd7513cae55
asifnaseebi <muhammadasifnaseebi@gmail.com>
2023-04-14 05:52:50 +0500

    youtube_scraping_uplode_t1

--- utils.py
+++ utils.py
@@ -1,36 +1,32 @@
 import time
 
+scroll_time = 5
 
-def scroll_to_page_end(driver):
+
+def scroll_all_pages(driver):
     last_height = driver.execute_script("return document.documentElement.scrollHeight")
     while True:
-        driver.execute_script("window.scrollTo(0, arguments[0]);", last_height)
-        time.sleep(1.5)
+        driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+        time.sleep(scroll_time)
         new_height = driver.execute_script("return document.documentElement.scrollHeight")
         if new_height == last_height:
             break
         last_height = new_height
 
 
-def extract_comment_data(soup):
-    all_content = soup.find_all('ytd-comment-thread-renderer')
-
-    data = []
-    for comment_data in all_content:
-        user_info = comment_data.find('ytd-comment-renderer').find('a', {'id': 'author-text'})
+def extract_comments_details(soup):
+    comments_segment = soup.find_all('ytd-comment-thread-renderer')
+    comments = []
+    for comment in comments_segment:
+        user_info = comment.find('ytd-comment-renderer').find('a', {'id': 'author-text'})
         user_name = user_info.text.strip()
-        comment_time = comment_data.find('a', {'class': 'yt-simple-endpoint style-scope yt-formatted-string'}).text
-        comment_text = comment_data.find('yt-formatted-string', {'class': 'style-scope ytd-comment-renderer'}).text
-        thumbnail_URL = comment_data.find('img', {'class': 'style-scope yt-img-shadow'}).get('src')
-        number_of_likes = comment_data.find(
-            'span', {'class': 'style-scope ytd-comment-action-buttons-renderer'}
-        ).text.strip()
-        all_comment_data = {
-            "user_name": user_name,
-            "comment_time": comment_time,
-            "number_of_likes": number_of_likes,
-            "comment_text": comment_text,
-            "thumbnail_URL": thumbnail_URL
-        }
-        data.append(all_comment_data)
-    return data
+        comment_text = comment.find('yt-formatted-string', {'id': 'content-text'}).text.strip()
+        likes = comment.find('span', {'id': 'vote-count-middle'}).text.strip()
+        comment_time = comment.find('a', {'class': 'yt-simple-endpoint style-scope yt-formatted-string'}).text.strip()
+        thumbnail_url_tag = comment.find('yt-img-shadow', {"class": "style-scope ytd-comment-renderer no-transition"})
+        if thumbnail_url_tag is not None:
+            thumbnail_url = thumbnail_url_tag.find('img').get('src')
+        else:
+            thumbnail_url = None
+        comments.append([user_name, comment_text, comment_time, likes, thumbnail_url])
+    return comments
